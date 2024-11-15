0f6dfdef98f0899b8e8e24ecc09ad493bf2c7db2
EdSerYo <89947206+EdSerYo@users.noreply.github.com>
2024-11-15 18:47:50 +0100

    Autoevaluacion 1

---
+++ POO/Unidad 1/Ejercicios/clases.py
@@ -0,0 +1,7 @@
+class Partido:
+    def __init__(self, eq_local, eq_visitante, ptos_local, ptos_visitante):
+        self.eq_local = eq_local
+        self.eq_visitante = eq_visitante
+        self.ptos_local = ptos_local
+        self.ptos_visitante = ptos_visitante
+        
--- POO/Unidad 1/Ejercicios/prueba.py
+++ POO/Unidad 1/Ejercicios/prueba.py
@@ -1,1 +1,6 @@
 print("HELLO WORD")
+
+from clases import Partido
+
+partido1 = Partido("A", "B", 100, 101)
+partido2 = Partido("C", "D", 99, 98)
