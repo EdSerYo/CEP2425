431cfc2cee4e9b1a899c342f62a2c31ce888c60a
EdSerYo <89947206+EdSerYo@users.noreply.github.com>
2024-11-15 19:44:36 +0100

    autoevaluacion 2

--- POO/Unidad 1/Ejercicios/clases.py
+++ POO/Unidad 1/Ejercicios/clases.py
@@ -1,7 +1,14 @@
 class Partido:
+    contador = 0
     def __init__(self, eq_local, eq_visitante, ptos_local, ptos_visitante):
         self.eq_local = eq_local
         self.eq_visitante = eq_visitante
         self.ptos_local = ptos_local
         self.ptos_visitante = ptos_visitante
         
+    def __str__(self):
+        return f"{self.eq_local}: {self.ptos_local} - {self.eq_visitante}: {self.ptos_visitante}"
+
+    @classmethod
+    def get_contador(c):
+        return f"El numero de partidoses: {c.contador}"
--- POO/Unidad 1/Ejercicios/prueba.py
+++ POO/Unidad 1/Ejercicios/prueba.py
@@ -4,3 +4,6 @@ from clases import Partido
 
 partido1 = Partido("A", "B", 100, 101)
 partido2 = Partido("C", "D", 99, 98)
+
+print(partido1.__str__())
+print(partido1.get_contador(Partido))
